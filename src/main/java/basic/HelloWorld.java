b75e7f59788e6a672e603e1f965ecef5c5c52095
Sudha S <sudhasaxena88@gmail.com>
2024-11-07 17:55:47 -0600

    added m1 method

---
+++ src/main/java/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	// from b1 branch
+	public void m1() {
+		System.out.println("from m1");
+	}
+
+}
